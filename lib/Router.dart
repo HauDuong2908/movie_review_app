e19a85b420a097d8008cc5e155eb33d53a8f1283
HauDuong2908 <dphau.20it2@vku.udn.vn>
2024-11-26 17:55:25 +0700

    login and get Started

--- lib/Router.dart
+++ lib/Router.dart
@@ -1,14 +1,19 @@
 import 'package:go_router/go_router.dart';
 import 'package:movie_review_app/Get_Started/Main_Started.dart';
+import 'package:movie_review_app/Login/login_ui.dart';
 
 class RouterWidget {
   static final GoRouter router = GoRouter(
-    initialLocation: '/Started',
+    initialLocation: '/started',
     routes: [
       GoRoute(
-        path: '/Started',
+        path: '/started',
         builder: (context, state) => Started(),
       ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginUi(),
+      )
     ],
   );
 }
